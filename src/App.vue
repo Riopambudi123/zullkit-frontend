f09bb12a6af58714071dc2b22308ae91ebef081e
Riopambudi123 <riopambudi879@gmail.com>
2023-01-25 22:14:03 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from "vue-router";
+
+import Navbar from "./components/layouts/Navbar.vue";
+</script>
+
+<template>
+  <Navbar />
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/css/styles.css";
+</style>
